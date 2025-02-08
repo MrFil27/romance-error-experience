5b1bff44ac81225412afb7782290caecd8a06f48
Filipeti <hinceanufilippo4@gmail.com>
2025-02-08 19:40:04 +0100

    Upload file

---
+++ js/index.js
@@ -0,0 +1,22 @@
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤";
+
+    document.body.appendChild(heart);
+
+    const left = Math.random() * 100;
+    heart.style.left = `${left}vw`;
+
+    const size = Math.random() * 40 + 20;
+    heart.style.fontSize = `${size}px`;
+
+    const duration = Math.random() * 3 + 2;
+    heart.style.animationDuration = `${duration}s`;
+
+    setTimeout(() => {
+        heart.remove();
+    }, duration * 1000);
+}
+
+setInterval(createHeart, 300);
