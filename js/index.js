2b13ecacd185849237e9e0398c22c288791f8426
MrFil27 <78660195+MrFil27@users.noreply.github.com>
2025-02-14 17:12:31 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function createHeart() {
     const heart = document.createElement("div");
     heart.classList.add("heart");
-    heart.innerHTML = "❤"/*"❤️"*/;
+    heart.innerHTML = "❤️"/*"❤"*/;
     //❤ uging mobile devices
     //❤️ using PC
 
